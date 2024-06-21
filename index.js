94d749fd653331f302b7127e9a42425f5fa99747
Saiful Islam Azad <saifulazad7061@gmail.com>
2024-06-21 19:26:22 +0600

    Shopery Server created and initialized and implemented

---
+++ index.js
@@ -0,0 +1,236 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); 
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+
+// Middle Wares
+app.use(cors());
+app.use(express.json());
+
+console.log("Username: ", process.env.DB_USER);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jip67yo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const usersCollection = client.db('Shopery').collection('users');
+    const categoriesCollection = client.db('Shopery').collection('categories');
+    const productsCollection = client.db('Shopery').collection('products');
+    const hotdealsCollection = client.db('Shopery').collection('hotdeals');
+    const cartsCollection = client.db('Shopery').collection('carts');
+    const wishListCollection = client.db('Shopery').collection('wishlist');
+    const blogsCollection = client.db('Shopery').collection('blogs');
+    const commentsCollection = client.db('Shopery').collection('comments');
+    const ordersCollection = client.db('Shopery').collection('orders');
+
+
+
+
+
+
+    // JWT Api
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '1h' });
+      res.send({ token });
+    });
+
+
+
+    // Middle Wares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+
+
+
+
+    // Users related API
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Already Exists", insertedId: null });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+
+      const insertedId = result.insertedId;
+      await usersCollection.updateOne(
+        { _id: insertedId },
+        { $set: { photo: user.photo } }
+      );
+      console.log(user);
+    });
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/categories', async (req, res) => {
+      const result = await categoriesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/blogs', async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/blogs/:id/comments", async (req, res) => {
+      const blogId = req.params.id;
+      const comment = req.body;
+      comment.blogId = new ObjectId(blogId);
+      comment.createdAt = new Date();
+      
+      const result = await commentsCollection.insertOne(comment);
+      const insertedComment = await commentsCollection.findOne({ _id: result.insertedId });
+
+      res.status(201).json(insertedComment);
+    });
+
+    app.get("/blogs/:id/comments", async (req, res) => {
+      const blogId = req.params.id;
+      const query = { blogId: new ObjectId(blogId) };
+      const comments = await commentsCollection.find(query).toArray();
+      res.send(comments);
+    });
+
+    // Cart APIs
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Wishlist APIs
+    app.post('/wishlist', async (req, res) => {
+      const wishItem = req.body;
+      const result = await wishListCollection.insertOne(wishItem);
+      res.send(result);
+    });
+
+    app.get('/wishlist', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await wishListCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/wishlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await wishListCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get('/hotdeals', async (req, res) => {
+      const result = await hotdealsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.status(400).send({ message: 'Email is required to delete cart items.' });
+      }
+    
+      const query = { email: email };
+      const result = await cartsCollection.deleteMany(query);
+    
+      if (result.deletedCount === 0) {
+        res.status(404).send({ message: 'No cart items found to delete.' });
+      } else {
+        res.send({ message: `Deleted ${result.deletedCount} cart items.` });
+      }
+    });
+
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+    
+      try {
+        const result = await ordersCollection.insertOne(order);
+        const insertedOrder = await ordersCollection.findOne({ _id: result.insertedId });
+
+        res.status(201).json({ message: 'Order placed successfully', order: insertedOrder });
+      } catch (error) {
+        console.error('Error placing order:', error);
+        res.status(500).json({ message: 'Failed to place order. Please try again.' });
+      }
+    });
+
+    app.get('/orders', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await ordersCollection.find(query).toArray();
+      res.send(result);
+  });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB Database!");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send("Shopery Server is Running......")
+});
+
+app.listen(port, () => {
+  console.log(`Shopery Server is running on port: ${port}`)
+});
