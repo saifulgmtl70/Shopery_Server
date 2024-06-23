9a580fe852c1b98f94bf70d7a613e3a5e02f7424
Saiful Islam Azad <saifulazad7061@gmail.com>
2024-06-23 08:39:16 +0600

    json webtoken, verifytoken, verifyadmin etc implemented

--- index.js
+++ index.js
@@ -42,31 +42,41 @@ async function run() {
 
 
 
-    // JWT Api
-    app.post('/jwt', async(req, res) =>{
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: '1h' });
-      res.send({ token });
-    });
-
-
-
-    // Middle Wares
-    const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers.authorization);
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorized access' });
-      }
-      const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.decoded = decoded;
-        next();
+      // jwt related api
+      app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
       })
-    }
+
+
+      const verifyToken = (req, res, next) => {
+        // console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
+
+
+      // use verify admin after verifyToken
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === 'admin';
+        if (!isAdmin) {
+          return res.status(403).send({ message: 'forbidden access' });
+        }
+        next();
+      }
 
 
 
@@ -94,8 +104,49 @@ async function run() {
       console.log(user);
     });
 
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    //get admin user
+    app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+
+      if(!email === req.decoded.email){
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      const query = {email: email};
+      const user =  await usersCollection.findOne(query);
+
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin })
+      
+    });
+
+
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+          $set: {
+              role: 'admin'
+          }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+
+  })
+
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -104,9 +155,27 @@ async function run() {
       res.send(result);
     });
 
+
+      // Menu Related API
+      app.post('/products',verifyToken, verifyAdmin, async (req,res) =>{
+        const item = req.body;
+        const result = await productsCollection.insertOne(item);
+        res.send(result);
+      });
+
+
     app.get('/products', async (req, res) => {
       const result = await productsCollection.find().toArray();
       res.send(result);
+    });
+
+
+    
+    app.delete('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
     });
 
     app.get('/blogs', async (req, res) => {
