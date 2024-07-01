188313a3a45c9014ff82dfe5a47935913649d739
Saiful Islam Azad <saifulazad7061@gmail.com>
2024-07-01 22:02:55 +0600

    Something new implemented

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ async function run() {
     const blogsCollection = client.db('Shopery').collection('blogs');
     const commentsCollection = client.db('Shopery').collection('comments');
     const ordersCollection = client.db('Shopery').collection('orders');
+    const billingsCollection = client.db('Shopery').collection('billings');
 
 
 
@@ -81,7 +82,19 @@ async function run() {
 
 
 
-
+    // Function to update user in the database
+    const updateUserInDatabase = async (userInfo) => {
+      const { displayName, photoURL, phone, email } = userInfo;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          displayName: displayName,
+          photoURL: photoURL,
+          phone: phone,
+        }
+      };
+      await usersCollection.updateOne(filter, updateDoc);
+    };
 
     // Users related API
     app.post("/users", async (req, res) => {
@@ -96,11 +109,6 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
 
-      const insertedId = result.insertedId;
-      await usersCollection.updateOne(
-        { _id: insertedId },
-        { $set: { photo: user.photo } }
-      );
       console.log(user);
     });
 
@@ -108,6 +116,21 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+
+
+    // New endpoint to update user
+    app.patch('/users', async (req, res) => {
+      const { displayName, photoURL, phone, email } = req.body;
+      try {
+        await updateUserInDatabase({ displayName, photoURL, phone, email });
+        res.status(200).send({ message: 'Profile updated successfully' });
+      } catch (error) {
+        console.error("Error updating user in database:", error);
+        res.status(500).send({ message: 'Error updating profile' });
+      }
+    });
+
+
 
 
     //get admin user
@@ -164,158 +187,173 @@ async function run() {
       });
 
 
-    app.get('/products', async (req, res) => {
-      const result = await productsCollection.find().toArray();
-      res.send(result);
-    });
+      app.get('/products', async (req, res) => {
+        const result = await productsCollection.find().toArray();
+        res.send(result);
+      });
 
 
-    app.patch('/products/:id', async (req, res) => {
-      const item = req.body;
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
-      const updatedDoc = {
-        $set: {
-          name: item.name,
-          category: item.category,
-          price: item.price,
-          sale: item.sale,
-          rating: item.rating,
-          availability: item.availability,
-          description: item.description,
-          images: item.images
+      app.patch('/products/:id', async (req, res) => {
+        const item = req.body;
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const updatedDoc = {
+          $set: {
+            name: item.name,
+            category: item.category,
+            price: item.price,
+            sale: item.sale,
+            rating: item.rating,
+            availability: item.availability,
+            description: item.description,
+            images: item.images
+          }
         }
-      }
 
-      const result = await productsCollection.updateOne(filter, updatedDoc)
-      res.send(result);
-    })
+        const result = await productsCollection.updateOne(filter, updatedDoc)
+        res.send(result);
+      })
 
 
     
-    app.delete('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+      app.delete('/products/:id', verifyToken, verifyAdmin, async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await productsCollection.deleteOne(query);
+          res.send(result);
+      });
+
+      app.get('/blogs', async (req, res) => {
+        const result = await blogsCollection.find().toArray();
+        res.send(result);
+      });
+
+      app.post("/blogs/:id/comments", async (req, res) => {
+        const blogId = req.params.id;
+        const comment = req.body;
+        comment.blogId = new ObjectId(blogId);
+        comment.createdAt = new Date();
+        
+        const result = await commentsCollection.insertOne(comment);
+        const insertedComment = await commentsCollection.findOne({ _id: result.insertedId });
+
+        res.status(201).json(insertedComment);
+      });
+
+      app.get("/blogs/:id/comments", async (req, res) => {
+        const blogId = req.params.id;
+        const query = { blogId: new ObjectId(blogId) };
+        const comments = await commentsCollection.find(query).toArray();
+        res.send(comments);
+      });
+
+      // Cart APIs
+      app.post('/carts', async (req, res) => {
+        const cartItem = req.body;
+        const result = await cartsCollection.insertOne(cartItem);
+        res.send(result);
+      });
+
+      app.get('/carts', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result);
+      });
+
+      app.delete('/carts/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
-        const result = await productsCollection.deleteOne(query);
+        const result = await cartsCollection.deleteOne(query);
         res.send(result);
-    });
-
-    app.get('/blogs', async (req, res) => {
-      const result = await blogsCollection.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/blogs/:id/comments", async (req, res) => {
-      const blogId = req.params.id;
-      const comment = req.body;
-      comment.blogId = new ObjectId(blogId);
-      comment.createdAt = new Date();
-      
-      const result = await commentsCollection.insertOne(comment);
-      const insertedComment = await commentsCollection.findOne({ _id: result.insertedId });
-
-      res.status(201).json(insertedComment);
-    });
-
-    app.get("/blogs/:id/comments", async (req, res) => {
-      const blogId = req.params.id;
-      const query = { blogId: new ObjectId(blogId) };
-      const comments = await commentsCollection.find(query).toArray();
-      res.send(comments);
-    });
-
-    // Cart APIs
-    app.post('/carts', async (req, res) => {
-      const cartItem = req.body;
-      const result = await cartsCollection.insertOne(cartItem);
-      res.send(result);
-    });
-
-    app.get('/carts', async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const result = await cartsCollection.find(query).toArray();
-      res.send(result);
-    });
-
-    app.delete('/carts/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await cartsCollection.deleteOne(query);
-      res.send(result);
-    });
+      });
 
     // Wishlist APIs
-    app.post('/wishlist', async (req, res) => {
-      const wishItem = req.body;
-      const result = await wishListCollection.insertOne(wishItem);
-      res.send(result);
-    });
+      app.post('/wishlist', async (req, res) => {
+        const wishItem = req.body;
+        const result = await wishListCollection.insertOne(wishItem);
+        res.send(result);
+      });
 
-    app.get('/wishlist', async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const result = await wishListCollection.find(query).toArray();
-      res.send(result);
-    });
+      app.get('/wishlist', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await wishListCollection.find(query).toArray();
+        res.send(result);
+      });
 
-    app.delete('/wishlist/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await wishListCollection.deleteOne(query);
-      res.send(result);
-    });
+      app.delete('/wishlist/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await wishListCollection.deleteOne(query);
+        res.send(result);
+      });
 
-    app.get('/hotdeals', async (req, res) => {
-      const result = await hotdealsCollection.find().toArray();
-      res.send(result);
-    });
+      app.get('/hotdeals', async (req, res) => {
+        const result = await hotdealsCollection.find().toArray();
+        res.send(result);
+      });
 
-    app.delete('/carts', async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        return res.status(400).send({ message: 'Email is required to delete cart items.' });
-      }
-    
-      const query = { email: email };
-      const result = await cartsCollection.deleteMany(query);
-    
-      if (result.deletedCount === 0) {
-        res.status(404).send({ message: 'No cart items found to delete.' });
-      } else {
-        res.send({ message: `Deleted ${result.deletedCount} cart items.` });
-      }
-    });
+      app.delete('/carts', async (req, res) => {
+        const email = req.query.email;
+        if (!email) {
+          return res.status(400).send({ message: 'Email is required to delete cart items.' });
+        }
+      
+        const query = { email: email };
+        const result = await cartsCollection.deleteMany(query);
+      
+        if (result.deletedCount === 0) {
+          res.status(404).send({ message: 'No cart items found to delete.' });
+        } else {
+          res.send({ message: `Deleted ${result.deletedCount} cart items.` });
+        }
+      });
 
-    app.post('/orders', async (req, res) => {
-      const order = req.body;
-      order.createdAt = new Date();
-    
-      try {
-        const result = await ordersCollection.insertOne(order);
-        const insertedOrder = await ordersCollection.findOne({ _id: result.insertedId });
+      app.post('/orders', async (req, res) => {
+        const order = req.body;
+        order.createdAt = new Date();
+      
+        try {
+          const result = await ordersCollection.insertOne(order);
+          const insertedOrder = await ordersCollection.findOne({ _id: result.insertedId });
 
-        res.status(201).json({ message: 'Order placed successfully', order: insertedOrder });
-      } catch (error) {
-        console.error('Error placing order:', error);
-        res.status(500).json({ message: 'Failed to place order. Please try again.' });
-      }
-    });
+          res.status(201).json({ message: 'Order placed successfully', order: insertedOrder });
+        } catch (error) {
+          console.error('Error placing order:', error);
+          res.status(500).json({ message: 'Failed to place order. Please try again.' });
+        }
+      });
 
-    app.get('/orders', async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const result = await ordersCollection.find(query).toArray();
-      res.send(result);
-    });
+      app.get('/orders', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await ordersCollection.find(query).toArray();
+        res.send(result);
+      });
 
 
-    app.delete('/orders/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await ordersCollection.deleteOne(query);
-      res.send(result);
-    });
+      app.delete('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await ordersCollection.deleteOne(query);
+        res.send(result);
+      });
+
+
+      app.post('/billings',  async (req,res) =>{
+        const item = req.body;
+        const result = await billingsCollection.insertOne(item);
+        res.send(result);
+      });
+
+
+       app.get('/billings', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await billingsCollection.find(query).toArray();
+        res.send(result);
+      });
 
 
     await client.db("admin").command({ ping: 1 });
